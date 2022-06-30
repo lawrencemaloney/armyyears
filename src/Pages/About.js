a7b37311c240bb4d4cdbce919e8bf610c948e2e3
Lawrence Maloney <lawrence.maloney@att.net>
2022-06-30 14:02:47 -0500

    update archive site

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -4,6 +4,7 @@ function About() {
   return (
     <div>
       <div className='mt-[300px]'>Archives page goes here</div>
+      <h1 className='mt-[300px]'>Archiving going on here</div>
     </div>
   );
 }
