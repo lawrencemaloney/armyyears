d774048f1678af5759416d024036583c25afb7a9
Lawrence Maloney <lawrence.maloney@att.net>
2022-07-10 15:08:51 -0500

    More iPhone fixes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,18 @@
-import React from "react";
-import Navbar from "./Navbar";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import MenuItems from "./MenuItems";import React, { useState } from "react";
+
 
 function Header() {
+  const [active, setActive] = useState(false);
+
+  const showMenu = () => {
+    setActive(!active);
+  };
+
   return (
     <>
+
       <div className='fixed  top-0 left-0 right-0 m-0 w-[95vw] ml-[2vw] bg-amber-50 lg:h-[185px] w-[98vw] justify-center  ml-[1vw] z-[1000] lg:space-x-36 flex text-center'>
         <img
           className='hidden  w-[60px] h-[62px]  lg:block '
@@ -24,9 +33,37 @@ function Header() {
           alt={"Pin"}
         />
       </div>
-    <div className='fixed z-1020 top-[135px] left-0 right-0 m-0 bg-[#fdf5e6]    '>
 
-      <Navbar />
+
+    <div className='fixed  top-[135px] left-0 right-0 m-0 bg-[#fdf5e6]    '>
+
+    <nav>
+        <ul className='hidden h-[55px]  fixed top-[185px] border w-[95vw] ml-[2.5vw] border-black gap-x-1  p-[2px] bg-amber-50 text-[22px]  italic justify-center z-[1000] items-center lg:flex lg:text-[26px]'>
+
+
+          <li className=' hover:text-black  hover:scale-[1.25]'>
+            <Link to='/Home'>Home</Link>
+          </li>
+          <li className=' hover:text-black  hover:scale-[1.25]'>
+            <Link to='/Training'>Training</Link>
+          </li>
+
+          <li className=' hover:text-black  hover:scale-[1.25]'>
+            <Link to='/InBerlin'>In Berlin</Link>
+          </li>
+          <li className=' hover:text-black  hover:scale-[1.25]'>
+            <Link to='/PhotoJ'>Photo Journalism</Link>
+          </li>
+          <li className=' hover:text-black  hover:scale-[1.25]'>
+            <Link to='/Archives'>Archives</Link>
+          </li>
+          <li className=' hover:text-black  hover:scale-[1.25]'>
+            <Link to='/About'>About</Link>
+          </li>
+        </ul>
+        <MenuItems showMenu={showMenu} active={active}/>
+      </nav>
+
 
     </div>
     </>
--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -7,25 +7,39 @@ function MenuItems({showMenu, active}) {
   return (
     <>
 
+{/* <AiOutlineClose className="h-[42px] w-[42px]"  onClick={showMenu}></AiOutlineClose> */}
 
-        <ul className={active ?'flex-col flex items-centered fixed inset-0 md:hidden ' : 'hidden' }>
-            <AiOutlineClose/>
-          <li className=' hover:text-black  '>
+
+{/* <AiOutlineMenu className="h-[32px] w-[32px] cursor-pointer"  onClick={showMenu}></AiOutlineMenu> */}
+
+<div className="absolute left-[50%] top-[45px]  cursor-pointer	-translate-x-1/2	-translate-y-1/2 ">
+
+<AiOutlineMenu className="h-[42px] w-[42px]"  onClick={showMenu}>
+  </AiOutlineMenu>
+</div>
+
+
+
+        <ul className={active ?'fixed z-20 top-[225px] w-[22ch] h-[55vh] flex-col left-1/2 bg-zinc-700/90 text-center	rounded  text-[32px]	 -translate-x-1/2	  md:hidden ' : 'hidden' }>
+
+          <li className=' hover:scale-[1.25] 	 text-white m-4  '>
             <Link to='/Home'>Home</Link>
           </li>
-          <li>
+
+
+          <li className=' hover:scale-[1.25] 	 text-white m-4  '>
             <Link to='/Training'>Training</Link>
           </li>
-          <li>
+          <li className=' hover:scale-[1.25] 	 text-white m-4  '>
             <Link to='/InBerlin'>In Berlin</Link>
           </li>
-          <li>
+          <li className=' hover:scale-[1.25] 	 text-white m-4  '>
             <Link to='/PhotoJ'>Photo Journalism</Link>
           </li>
-          <li>
+          <li className=' hover:scale-[1.25] 	 text-white m-4  '>
             <Link to='/Archives'>Archives</Link>
           </li>
-          <li>
+          <li className=' hover:scale-[1.25] 	 text-white m-4  '>
             <Link to='/About'>About</Link>
           </li>
         </ul>
