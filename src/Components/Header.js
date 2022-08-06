a7dfdc31c46263ed9ba7c3b0ac5aec5755ed83eb
Lawrence Maloney <lawrence.maloney@att.net>
2022-08-06 15:09:11 -0500

    QF8

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,7 +17,7 @@ export default function Header() {
   return (
     <>
 
-      <div className=' top-0 left-0 right-0 m-0 h-[230px] w-[100vw] md:ml-[2vw] bg-amber-50  justify-center border-b	sm:border-none border-black  sm:ml-[1vw] z-[100]  sm:h-[186px] lg:space-x-36 flex text-center'>
+      <div className=' top-0 left-0 right-0 m-0 h-[130px] w-[100vw] md:ml-[2vw] bg-amber-50  justify-center   sm:ml-[1vw] z-[100]  sm:h-[186px] lg:space-x-36 flex text-center'>
  
         <img
           className='  hidden sm:w-[50px] sm:h-[53px] sm:flex absolute left-16 top-12 '
@@ -37,22 +37,19 @@ export default function Header() {
           height={60}
           width={60}
           alt={"Pin"}
-        />
-          <div className="    sm:hidden top-[170px] sm:right-16 sm:top-12  cursor-pointer  ">
-
-          <AiOutlineMenu className="fixed left-5 top-5 h-[32px] w-[32px] ring-offset-2 ring-2	 z-[1000]  bg-white"  onClick={showHideMenu}>
-          </AiOutlineMenu>
-          </div>
-
-          <MenuItems showHideMenu={showHideMenu} active={active}/>
-
+          />
       </div>
 
 
-
-
-
     <div className=' sticky  top-0 left-0 right-0 m-0 bg-[#fdf5e6]    '>
+
+    <div className="    sm:hidden top-[170px] sm:right-16 sm:top-12  py-4 cursor-pointer border-black border-b ">
+
+    <AiOutlineMenu className=" sticky left-[50%]  -translate-x-1/2 top-[35px] h-[32px] w-[32px] ring-offset-2 ring-2	 z-[1000]  bg-white"  onClick={showHideMenu}>
+    </AiOutlineMenu>
+    <MenuItems showHideMenu={showHideMenu} active={active}/>
+
+    </div>
 
     <nav>
         <ul className='hidden h-[55px]  border w-[100vw]  border-black sm:gap-x-0 sm:space-x-6 p-[2px] lg:gap-x-6 lg:space-x-12 p-[2px] bg-amber-50 text-[18px] lg:text-[24px] italic justify-center z-[1000] items-center sm:flex '>
