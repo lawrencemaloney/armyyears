aabb48471da09c726ac3cfb612742958e4cde3bb
Lawrence Maloney <lawrence.maloney@att.net>
2022-08-13 15:43:33 -0500

    Changes 08 13 22

--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -7,7 +7,7 @@ function MenuItems({showHideMenu, active}) {
   return (
     <>
 
-        <ul className={active ?'fixed z-20 	left-[50%] transform -translate-x-[50%]  duration-500 ease-in w-[18ch] h-[24rem] flex-col  bg-zinc-700/90 text-center	rounded-lg  text-[28px] 	lg:hidden ' : ' transform translate-x-[50%]  duration-500 ease-in text-[28px] w-[18ch] absolute left-[100%] text-center' }>
+        <ul className={active ?'fixed z-20 	left-[50%] transform -translate-x-[50%]  duration-500 ease-in w-screen	 h-[24rem] flex-col  bg-zinc-700/90 text-center	rounded-lg  text-[28px] 	lg:hidden ' : ' transform translate-x-[50%]  duration-500 ease-in text-[28px] w-[18ch] absolute left-[100%] text-center' }>
 
           <li onClick={() => showHideMenu()} className=' hover:scale-[1.1] 	 text-white m-4  '>
             <Link to='/Home'>My Story</Link>
