7d9c5c5669a8fc82ee12b5f8c9ef11920c3c3b13
Larry Maloney <55635508+lawrencemaloney@users.noreply.github.com>
2022-11-13 16:46:49 -0600

    More fixes to Photos for iPhone

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -41,35 +41,25 @@ const ImageSlider = ({ setSelectedImg, selectedImg,  filteredImages }) => {
     <>
     <div className="backdrop z-[21] fixed  w-[100%] h-[100%] bg-black overflow-hidden	 justify-center items-center inset-0 " >
     {/* <div className="backdrop z-[21] fixed top-0 left-0 w-[100%] h-[100%] bg-[#bdb9af]   " > */}
-    <AiOutlineCloseSquare className='absolute text-white  top-6 cursor-pointer right-[30px]  mx-auto h-[36px] w-[36px]' onClick={handleClick}/>
+    <AiOutlineCloseSquare className='absolute text-white  top-6 cursor-pointer right-[30px]   h-[36px] w-[36px]' onClick={handleClick}/>
+    <h1 className=' my-6  text-[24px]'>{filteredImages[currentIndex].caption}</h1> 
 
-    
-      <div className=" mx-auto text-white object-center  object-contain  static  grid  grid-cols-6 gap-4">
-
-        <img className=" m-auto h-screen col-start-1 col-span-4 object-contain 	 " src={filteredImages[currentIndex].url} alt={filteredImages[currentIndex].caption} />
+      <div className=" mx-auto text-white object-center  object-contain  static  flex-1 flex-col  ">
+        <img className="  h-[60%]  object-contain my-8 " src={filteredImages[currentIndex].url} alt={filteredImages[currentIndex].caption} />
+      </div>
         
-      <div className="col-start-5 col-span-2  mt-40 w-[200px]">
-          <h1 className='ml-[30px]  mt-2 z-[19] w-[400px]  text-[34px]'>{filteredImages[currentIndex].caption}</h1> 
-
+      {/* <div className=" w-[200px] "> */}
             
-          <div className="flex flex-row fixed top-[520px] items-center ">
-            <AiOutlineCaretLeft className=' z-[19] text-white  h-[54px] w-[54px] cursor-pointer' onClick={goToPrevious}/>  
+          <div className="flex flex-row items-center ">
+            <AiOutlineCaretLeft className=' z-[19] text-white   h-[24px] w-[24px] cursor-pointer' onClick={goToPrevious}/>  
 
-            <p className='justify-items-center text-center m-1 text-xl'>{currentIndex+1} of {imgArrayLen} </p>
-            
-
-
-            <AiOutlineCaretRight className=' z-[19] text-white   h-[54PX] w-[54px] cursor-pointer' onClick={goToNext}/>
-
-
-            
-
+            <p className='justify-items-center text-[10px] text-center m-1 text-xl'>{currentIndex+1} of {imgArrayLen} </p>
+            <AiOutlineCaretRight className=' z-[19] text-white   h-[24px] w-[24px] cursor-pointer' onClick={goToNext}/>
           </div>
 
-      </div>
+      {/* </div> */}
 
 
-      </div>
 
     </div>
     </>
