0133b96623df155d48664b427dbf86174ca478d4
Larry Maloney <55635508+lawrencemaloney@users.noreply.github.com>
2022-11-21 17:16:53 -0600

    Firsdt fix 11 21 22

--- src/Pages/MyPhotos.js
+++ src/Pages/MyPhotos.js
@@ -43,19 +43,62 @@ function MyPhotos() {
 	<>
 
 
-		<div className='App bg-black w-[100vw]  text-white '>
+		{/* <div className='App bg-black w-[100vw]  text-white '> */}
+		<div className="subhead  text-white   z-[32] sticky w-screen bg-black h-[140px] top-0 ">
 
-			
+			<h1 className='  mb-[10px] text-center text-3xl'>My Photos</h1>
 
-			<div className="pageContainer grid  grid-cols-2   auto-rows-max	 gap-4 w-[100vw]	
-			sm:grid-cols-4  ">
 
-				<div className="subhead z-[21] sticky w-screen bg-black h-[140px] top-0">
-					<h1 className='  mt-[2rem] mb-[10px] text-center text-3xl  
-					sm:text-4xl w-[100vw]   md:fixed md:top-[30px]'>My Photos</h1>
+		</div>
 
-					<h2 className=' text-2xl col-start-1 col-span-12 text-center sm:hidden'>{group}</h2>
-					{!isOpen ?
+			<div className="pageContainer bg-black text-white flex w-[100vw]   ">
+
+				
+				<div className={`groups p-4 w-[160px]  bg-black text-white  overflow-scroll  h-[100%] text-[16px]  ${isOpen ? 'translate-x-0 ' : 'translate-x-full sm:translate-x-0'} ease-in-out duration-700`}>
+
+				<ul className='  h-[100%] w-[120px] flex-col mt-4   bg-black text-left rounded-lg text-white   text-[14px]  space-y-[10px] '>
+					<p className=' '>Photo Categories</p>
+
+					<GroupButton name="Army" groupActive={group === 'Army' ? true : false} handleSetgroup={setGroup} />  
+
+					<GroupButton name="By Request" groupActive={group === 'By Request' ? true : false} handleSetgroup={setGroup} /> 
+
+					<GroupButton name="Berlin" groupActive={group === 'Berlin' ? true : false} handleSetgroup={setGroup} />
+
+					<GroupButton  name="Amsterdam" groupActive={group === 'Amsterdam' ? true : false} handleSetgroup={setGroup} /> 
+
+					<GroupButton name="Ferry" groupActive={group === 'Ferry' ? true : false} handleSetgroup={setGroup} />
+					<GroupButton name="London" groupActive={group === 'London' ? true : false} handleSetgroup={setGroup} />
+					<GroupButton name="Oxford" groupActive={group === 'Oxford' ? true : false} handleSetgroup={setGroup} />
+
+					<GroupButton name="All" groupActive={group === 'All' ? true : false} handleSetgroup={setGroup} />
+
+					{/* </div> */}
+
+					<p>Navigation</p>
+					<li className=' hover:scale-[1.1] text-left ml-[10px] '>
+					<Link to='/Home'>My Story</Link>
+					</li>
+					<li className=' hover:scale-[1.1]text-left ml-[10px]'>
+					<Link to='/Training'>Training</Link>
+					</li>
+					<li className=' hover:scale-[1.1]  text-left ml-[10px] '>
+					<Link to='/InBerlin'>In Berlin</Link>
+					</li>
+					<li className=' hover:scale-[1.1]  text-left ml-[10px] '>
+					<Link to='/BOarticles'>My Journalism</Link>
+					</li>
+					<li className=' hover:scale-[1.1] text-left ml-[10px] '>
+					<Link to='/MyPhotos'>My Photos</Link>
+					</li>
+					<li className=' hover:scale-[1.1]  text-left ml-[10px]'>
+					<Link to='/Archives'>Archives</Link>
+					</li>
+					<li className=' hover:scale-[1.25]  text-left ml-[10px]'>
+					<Link to='/About'>About</Link>
+					</li>
+					</ul>
+				{!isOpen ?
 					(
 						<button onClick={() => setIsOpen(!isOpen)}>
 							<AiOutlineMenu className=" absolute  top-2 right-2 h-[32px] w-[32px]  hover:scale-[1.2] cursor-pointer	z-[35] bg-#fffbeb sm:hidden " >
@@ -70,93 +113,72 @@ function MyPhotos() {
 						</button>    
 					)}
 				</div>
+		
+		<div className='textimagecontainer'>	
+				<div className="textcontainer mt-[2rem]  mx-2  text-[20px] leading-[32px]  
+				sm:min-w-[65ch]      ">
+
+					<h2 className=' text-2xl text-center place-self-center	   p-6 '>{group} Photos</h2>
 
 
 
-				<div className={`groups p-4 w-screen  bg-black text-white flex flex-col fixed top-10  rounded-lg overflow-scroll z-[20] h-[1600px] text-[16px]
-
-				sm:ml-[4px] sm:h-[650px] sm:w-[150px]  sm:top-[180px] sm:left-0 md:ml-[50px]  sm:text-[20px]
-				 	${isOpen ? 'translate-x-0 ' : 'translate-x-full sm:translate-x-0'} ease-in-out duration-700`}>
-
-					<ul className='  h-[100%] w-[100px] flex-col mt-4   bg-black text-left rounded-lg text-white   text-[14px]  space-y-[10px] '>
-						<p className=' '>Photo Categories</p>
-
-						<GroupButton name="Army" groupActive={group === 'Army' ? true : false} handleSetgroup={setGroup} />  
-
-						<GroupButton name="By Request" groupActive={group === 'By Request' ? true : false} handleSetgroup={setGroup} /> 
-
-						<GroupButton name="Berlin" groupActive={group === 'Berlin' ? true : false} handleSetgroup={setGroup} />
-
-						<GroupButton  name="Amsterdam" groupActive={group === 'Amsterdam' ? true : false} handleSetgroup={setGroup} /> 
-
-						<GroupButton name="Ferry" groupActive={group === 'Ferry' ? true : false} handleSetgroup={setGroup} />
-						<GroupButton name="London" groupActive={group === 'London' ? true : false} handleSetgroup={setGroup} />
-						<GroupButton name="Oxford" groupActive={group === 'Oxford' ? true : false} handleSetgroup={setGroup} />
-
-						<GroupButton name="All" groupActive={group === 'All' ? true : false} handleSetgroup={setGroup} />
-
-						{/* </div> */}
-
-						<p>Navigation</p>
-						<li className=' hover:scale-[1.1] text-left ml-[10px] '>
-						<Link to='/Home'>My Story</Link>
-						</li>
-						<li className=' hover:scale-[1.1]text-left ml-[10px]'>
-						<Link to='/Training'>Training</Link>
-						</li>
-						<li className=' hover:scale-[1.1]  text-left ml-[10px] '>
-						<Link to='/InBerlin'>In Berlin</Link>
-						</li>
-						<li className=' hover:scale-[1.1]  text-left ml-[10px] '>
-						<Link to='/BOarticles'>My Journalism</Link>
-						</li>
-						<li className=' hover:scale-[1.1] text-left ml-[10px] '>
-						<Link to='/MyPhotos'>My Photos</Link>
-						</li>
-						<li className=' hover:scale-[1.1]  text-left ml-[10px]'>
-						<Link to='/Archives'>Archives</Link>
-						</li>
-						<li className=' hover:scale-[1.25]  text-left ml-[10px]'>
-						<Link to='/About'>About</Link>
-						</li>
-						</ul>
-				</div>
-
-				<div className="textcontainer mt-[2rem] w-[100vw] mx-2  text-[20px] leading-[32px] col-start-1 col-span-2 row-start-2  h-[25%]
-				sm:min-w-[65ch] sm: sm:text-[25px] sm:max-w-[20ch] sm:p-[20px] sm:col-start-2  sm:col-span-5  sm:row-start-2  
-				md:mx-auto
-				lg:max-w-[28ch]      ">
-
+					{group === 'Army' && (
+						<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					These are some of the photos I took while in the Army as part of my job as Battalion PIO clerk. Lots of award presentations, baseball coverage, official Army ceremonies, field training exercises, and miscellaneous pictures taken while covering those events. I particularly enjoyed taking pictures of the Brigade baseball games.
+					</p>
+					) }
+					{group === 'By Request' && (
 					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
-					These are some of the photos I took while in the Army.  Those labeled "Army" were taken as part of my job as Battalion PIO clerk. Lots of award presentations, baseball coverage, official Army ceremonies, field training exercises, and miscellaneous pictures taken while covering those events. I particularly enjoyed taking pictures of the Brigade baseball games.
+					By Request Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
 					</p>
-
-
-					{/* <p>
-					I am also pleased with some of the dramatic pictures I took while our battalion was in training in
-						Hoenfels in southern Germany. 
-						The aerial pictures were taken as I was sitting facing the open door of a Bell UH-1
-						"Huey" helicopter so I could get clear shots of the terrain. The pilot for the training recon mission
-						had recently returned from his latest tour in Vietnam (he had several). I was ostensibly held into my
-						seat by a single very thin seatbelt but I doubt if it would have held my weight if I was relying on it
-						alone. I had a headset and microphone so I could communicate with the crew. At one point, the pilot
-						asked if I would like to experience some of the maneuvers that were used in Vietnam. Being 23 years old,
-						I of course said "absolutely!" He then throttled up and laid the chopper over on its side or at least as
-						close to 90 degrees as was possible. At an altitude of around 1K feet, watching the ground go by at high
-						speed was exhilarating, to say the least. 
+					) }
+					{group === 'Berlin' && (
+					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					BERLIN Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
 					</p>
+					) }
+					{group === 'Amsterdam' && (
+					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					Amsterdam Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
+					</p>
+					) }
+					{group === 'Ferry' && (
+					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					FERRY Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
+					</p>
+					) }
+					{group === 'London' && (
+					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					LONDON Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
+					</p>
+					) }
+					{group === 'Oxford' && (
+					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					OXFORD Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
+					</p>
+					) }
+					{group === 'All' && (
+					<p className='p-2  text-[20px] md:max-w-[68ch] sm:text-[25px] sm:max-w-[48ch] leading-[32px]  md:max-w-[68ch] 	'>
+					All Photos ==== ipsum dolor sit amet consectetur, adipisicing elit. Omnis eveniet soluta inventore minus atque, sapiente optio consectetur rem culpa illum quisquam hic dolore harum, expedita distinctio vel, fuga obcaecati dicta.
+					</p>
+					) }
+
+					
+				
+
+				
+						{/* {group === 'By Request' ? */}
+				
 					
 					
-					<p>
-					Word got out that I could take decent photos and I had many requests to take portrait photos for friends. I was even asked to be the photographer for one Army officer's wedding. My popularity was due, in no small part, to the fact that I didn't charge for my services.
-					</p> */}
 				</div>
-					{/* <div className="groups ml-[4px]  p-4 max-h-[620px] w-[100px] bg-black text-white  flex flex-col fixed top-[320px] rounded-lg overflow-y-scroll scrollbar"> */}
+
 
 
 				
-					<div className="imgcontainer ml-[20px] grid grid-cols-2 col-start-1 col-span-2 row-start-4  sm:grid-cols-3 sm:col-start-2  sm:col-span-6        md:col-start-2  md:col-span-6   lg:grid-cols-4  lg:col-start-2  lg:col-span-4   ">
+					<div className="imgcontainer ml-[20px] grid grid-cols-2 sm:grid-cols-3 gap 4   ">
 						
+
 						
 					{filteredImages.map(image => (
 
@@ -179,7 +201,8 @@ function MyPhotos() {
 
 				{/* </SRLWrapper> */}
 			</div>
-		</div>
+			</div>
+		{/* </div> */}
 	</>
 	);
 }
