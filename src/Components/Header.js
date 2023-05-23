25a9313cde98e1d3015ce3859806e7f70e93b29c
Larry Maloney <55635508+lawrencemaloney@users.noreply.github.com>
2023-05-23 12:32:32 -0500

    REl 1 05 23 23

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,14 +4,12 @@ export default function Header() {
   return (
     <>
       {/* <div className="header mt-0 h-[100%] border border-black flex flex-col items-center"> */}
-        <div className="banner flex flex-col h-[220px] text-yellow-300	bg-[#283618]  border border-b-2 border-black justify-center    ">
+        <div className="banner flex flex-col h-[220px] border border-b-2 border-black justify-center mt-6   ">
 
-
-          <h1 className=" text-center  mt-10   text-4xl text-bold   ">
+          <h1 className=" text-center  mt-20 ipx:mt-0  text-4xl text-bold italic font-medium  ">
           An Unexpected Adventure {" "}
             <p className="mt-[20px] text-2xl">Larry Maloney 1969 to 1972</p>
-          </h1>
-
+            </h1>
 
         </div>
 
