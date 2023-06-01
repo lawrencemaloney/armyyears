7e0684cb6436fa121794bb80c3880000cd820b28
Larry Maloney <55635508+lawrencemaloney@users.noreply.github.com>
2023-06-01 16:59:59 -0500

    Rel 1 6 1 234

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -14,7 +14,7 @@ export default function Sidebar() {
     {!isOpen ? 
     (
         <FaBars 
-          className="mobileopen fixed text-black  z-30 flex items-center tablet:w-[32px] tablet:h-[32px] cursor-pointer h-[18px] w-[18px]  right-2 top-2 font-light	"
+          className="mobileopen fixed text-black  z-30 flex items-center tablet:w-[32px] tablet:h-[32px] cursor-pointer h-[18px] w-[18px]  right-2 top-2 	stroke-0 fill-red-600"
           onClick={() => setIsOpen(!isOpen)}>
         </FaBars> 
 
@@ -22,7 +22,7 @@ export default function Sidebar() {
     :
     (
       <FaTimes 
-      className="mobileclose flex text-4xl text-black  items-center cursor-pointer fixed right-2 top-2 z-50 w-[18px] h-[18px] tablet:w-[32px] tablet:h-[32px]"
+      className="mobileclose flex  text-black  items-center cursor-pointer fixed right-2 top-2 z-50 w-[18px] h-[18px]  tablet:w-[32px] tablet:h-[32px]"
       onClick={() => setIsOpen(!isOpen)}>
     </FaTimes>
 
@@ -30,9 +30,9 @@ export default function Sidebar() {
 }
 
 <div
-  className={`navbar top-0 right-0 w-full tablet:w-2/5   p-10 bg-[#ccc9bc]  fixed h-full z-40  ${isOpen ? "translate-x-0" : "translate-x-full  "} ease-in-out duration-300`}>
+  className={`navbar top-0 right-0 w-full tablet:w-1/4   p-10 bg-[#ccc9bc]  fixed h-full z-40  ${isOpen ? "translate-x-0" : "translate-x-full  "} ease-in-out duration-300`}>
 
-<p className="-mt-6 font-semibold text-[32px] ">Pages</p>
+<p className="-mt-6  text-[32px] ">Pages</p>
 
 <nav className="">
  <div className="flex flex-col mt-2 ml-8  space-y-2 text-2xl landscape:grid landscape:grid-cols-2 landscape:mt-6 mobile:landscape:flex ">
@@ -40,7 +40,7 @@ export default function Sidebar() {
  
            <CustomLink to="/Home">The Lottery</CustomLink>
 
-           <CustomLink to="/Training">Training</CustomLink>
+           <CustomLink to="As/Training">Training</CustomLink>
 
            <CustomLink to="/InBerlin">In Berlin</CustomLink>
 
