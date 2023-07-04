6292e5f5fdc748c6dc3f9403fb1f6b66a55025e1
Larry Maloney <55635508+lawrencemaloney@users.noreply.github.com>
2023-07-04 17:33:48 -0500

    Rel 4 on 7 4 23

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      
       padding: {
-        '1/3': '33/333333%',
-        '2/3': '66.6666',
+        "1/3": "33/333333%",
+        "2/3": "66.6666",
       },
       screens: {
         ipx: "425px",
@@ -15,13 +14,18 @@ module.exports = {
         laptop: "1070",
         desktop: "1150px",
         xldesktop: "1440px",
-        'portrait': {
-          'raw': '(orientation: portrait)'
+        portrait: {
+          raw: "(orientation: portrait)",
+        },
+        landscape: {
+          raw: "(orientation: landscape)",
+        },
       },
-       'landscape': {
-          'raw': '(orientation: landscape)'
-        }}
     },
   },
   plugins: [require("tailwind-scrollbar")],
+  "postcss-import": {},
+  "tailwindcss/nesting": "postcss-nesting",
+  tailwindcss: {},
+  autoprefixer: {},
 };
